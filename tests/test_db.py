af3651bc8c1fc2d77c78a680b6a2dda30438b674
“Michelle <itsmichellelin@gmail.com>
2023-07-19 14:41:45 -0700

    add more test cases for testing database

--- tests/test_db.py
+++ tests/test_db.py
@@ -30,16 +30,37 @@ class TestTimelinePost(unittest.TestCase):
         test_db.close()
 
     def test_timeline_post(self):
+        # Verify there are 0 timeline posts
+        total = TimelinePost.select()
+        assert len(total) == 0
+        
         # Create 2 timeline posts.
         first_post = TimelinePost.create(name='John Doe', email='john@example.com', content='Hello World, I\'m John!')
         assert first_post.id == 1
         second_post = TimelinePost.create(name='Jane Doe', email='jane@example.com', content='Hello World, I\'m Jane!')
         assert second_post.id == 2
-        # TODO: Get timeline post and assert that they are correct
-        posts = {
-        'timeline_posts' : [
-            model_to_dict(p)
-            for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
-            ]
-        }
-        print(posts)
+        
+        # Get timeline posts and assert that they are correct
+        first = TimelinePost.get(TimelinePost.id == 1)
+        assert first.name == 'John Doe'
+        assert first.email == 'john@example.com'
+        assert first.content == 'Hello World, I\'m John!'
+        
+        second = TimelinePost.get(TimelinePost.id == 2)
+        assert second.name == 'Jane Doe'
+        assert second.email == 'jane@example.com'
+        assert second.content == 'Hello World, I\'m Jane!'
+        
+        # Verify there are 2 timeline posts
+        total = TimelinePost.select()
+        assert len(total) == 2
+        
+        # Delete timeline post and verify only 1 left
+        second.delete_instance()
+        total = TimelinePost.select()
+        assert len(total) == 1
+        
+        # Delete last timeline post and verify none are left
+        first.delete_instance()
+        total = TimelinePost.select()
+        assert len(total) == 0
