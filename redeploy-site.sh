1e52c3203a4bab9075bcd51d84a12c2f161f6138
Miguel Ángel Mireles Vázquez <mamv3x3@gmail.com>
2023-07-26 17:40:12 -0600

    UPDATE redeploy-site script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -6,13 +6,11 @@ cd ~/miguel-mireles-pe-portfolio-site/
 # Step 2: Update the git repository with the latest changes
 git fetch && git reset origin/main --hard
 
-# Step 3: Activate the Python virtual environment and install dependencies
-python -m venv python3-virtualenv
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
+# Step 3: Spin containers down to prevent out of memory issues on our VPS instances when building
+docker compose -f docker-compose.prod.yml down
 
-# Step 4: Restart myportfolio service
-systemctl restart myportfolio
+# Step 4: Build the containers with the latest changes and spin them back up
+docker compose -f docker-compose.prod.yml up -d --build
 
 # Print a success message
 echo "Site redeployed successfully!"
